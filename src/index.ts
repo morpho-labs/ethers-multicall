08844ace3b79de46c1e860552c0b21fa61f0dd9a
Rubilmax <rmilon@gmail.com>
2022-09-01 11:55:51 +0200

    fix(types): export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { EthersMulticall } from "./ethers";
+export type { ContractCall, MulticallCallbackHooks } from "./ethers";
 export { MULTICALL_ADDRESSES } from "./registry";
 export type { IMulticallWrapper } from "./interface";
