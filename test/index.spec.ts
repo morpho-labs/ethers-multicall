232594210971d29b96e0d890eb025fd8c0247d6d
Rubilmax <rmilon@gmail.com>
2023-01-10 22:02:25 +0100

    fix(test): revert tdd

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -176,18 +176,5 @@ describe("ethers-multicall", () => {
         )
       );
     });
-
-    // it("should retry with half payload when query too large", async () => {
-    //   const multicall = new EthersMulticall(rpcProvider);
-    //   const wrappedUni = multicall.wrap(uni);
-
-    //   await Promise.all(
-    //     _range(100_000).map((i) => {
-    //       const address = utils.hexZeroPad("0x" + i.toString(16), 20);
-
-    //       return wrappedUni.allowance(address, address);
-    //     })
-    //   );
-    // });
   });
 });
